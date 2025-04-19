17ffc6f67761df8f20ec75b65ecfd1282b8980f2
Fabio GUERREIRO MARQUES <fabio.g-marques@hotmail.com>
2025-04-19 16:04:33 +0200

    Ajout de la classe Troupe

---
+++ src/Troupe.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Troupe {
+
+    private ArrayList<Soldat> listeDeSoldat;
+
+    public Troupe() {
+        listeDeSoldat = new ArrayList<Soldat>();
+    }
+
+    public void ajouterSoldat(Soldat soldat){
+        listeDeSoldat.add(soldat);
+    }
+
+    public void retirerSoldat(){
+        listeDeSoldat.remove(listeDeSoldat.get(listeDeSoldat.size()-1));
+    }
+
+    public String toString() {
+        for(Soldat s : listeDeSoldat){
+            System.out.println(s.toString());
+        }
+        return null;
+    };
+}
